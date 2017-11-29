e5032d5c329eee2dc469d3f8c38b681f7682ad5c
sethshaffer <sshaffer@granburyrs.com>
2017-11-29 15:18:24 -0500

    Initial commit

---
+++ src/test/java/com/shaffer/LibraryTest.java
@@ -0,0 +1,12 @@
+package com.shaffer;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+    @Test
+    public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
